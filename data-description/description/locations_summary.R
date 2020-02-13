dce5270604a363837a64982bcd11df0b25ce039c
Dmitry Sedov <d-sedov@outlook.com>
2020-02-13 17:48:57 -0600

    Descriptive evidence tables produced.

---
+++ data-description/description/locations_summary.R
@@ -0,0 +1,198 @@
+################################################################################
+################################################################################
+#
+# FILE: locations-summary.R
+#
+# BY: Dmitry Sedov 
+#
+# CREATED: Thu Feb 13 2020
+#
+# DESC: This file creates a table with locations (CBGs / CTs) summary statistics.
+#
+################################################################################
+################################################################################
+
+
+################################ Libraries #####################################
+
+library(readr)
+library(dplyr)
+library(xtable)
+library(kableExtra)
+library(jsonlite)
+library(tidyr)
+library(ggplot2)
+library(scales)
+
+################################################################################
+
+################################# Options ######################################
+
+# Set the tables properties
+# Footnote font size
+options(xtable.size = 'footnotesize')
+# Page top placement
+options(xtable.table.placement = 't')
+
+# Input folder
+input_folder_path = '/Users/muser/dfolder/Research/urban/data/output/descriptive'
+
+# Output folders
+tables_folder_path = '/Users/muser/dfolder/Research/urban/output/tables/descriptive'
+plots_folder_path = '/Users/muser/dfolder/Research/urban/output/plots/descriptive'
+
+# Visits data vintage
+year = '2018'
+month = 'October'
+
+# My theme for plots 
+my_theme <- theme(legend.text = element_text(size = 14),
+                  legend.title = element_text(size= 16),
+                  plot.title = element_text(hjust = 0.5, size = 18)
+)
+mycolorscheme1 <- c('black', 'orange', 'purple')
+mycolorscheme2 <- c('blue', 'red', 'darkgreen')
+mycolorscheme3 <- c('#e70300', '#00279a', '#009500', '#ffe200', '#722ab5')
+
+################################################################################
+
+################################# Functions ####################################
+
+# This function computes the summary statistics for a variable
+simple_summary <- function(data, var) {
+    require(dplyr)
+    require(tibble)
+    data %>% 
+        select(!!sym(var)) %>% 
+        summarise_all(list(perc_na = function(x){100 * 
+                sum(is.na(x)) / length(x)},
+                q10 = ~quantile(., 0.1, na.rm = T),
+                q25 = ~quantile(., 0.25, na.rm = T), 
+                median = ~median(as.numeric(.), na.rm = T), 
+                q75 = ~quantile(., 0.75, na.rm = T), 
+                q90 = ~quantile(., 0.9, na.rm = T),
+                mean = ~mean(., na.rm = T), 
+                sd = ~sd(., na.rm = T)
+        )
+        ) %>%
+        add_column(v = var, .before = 1)
+}
+
+################################################################################
+
+############################## Prepare data ####################################
+
+# Import data
+cbgs <- read_csv(file.path(input_folder_path, 'data_cbg.csv'))
+
+# Replace missing values with 0 (no restaurants / establishments in cbgs)
+cbgs <- cbgs %>% 
+    mutate(urban = !is.na(cbsa),
+           rest_number = replace(rest_number, 
+                                 which(is.na(rest_number)),
+                                 0),
+           est_number = replace(est_number,
+                                which(is.na(est_number)),
+                                0),
+           rest_area = replace(rest_area, 
+                               which(is.na(rest_area)),
+                               0),
+           est_area = replace(est_area,
+                              which(is.na(est_area)),
+                              0),
+           area_km2 = area_m2 / 1000000
+    )
+
+# Select variables for summary statistics construction
+vars <- list('area_km2',
+             'rest_number',
+             'rest_area',
+             'est_number',
+             'est_area'
+)
+
+# Variable names for presentation
+vars_names <- c('Area (sq. km.)',
+                'Restaurant number', 
+                'Total rest. area (sq. m.)',
+                'Establishment number', 
+                'Total estab. area (sq. m.)'
+                )
+
+cbgs_urban <- cbgs %>% filter(urban)
+# Create urban-locations summary table
+cbgs_urban_summary_table <- bind_rows(lapply(vars,
+                                              function(x) {
+                                                  simple_summary(cbgs_urban, x)
+                                              }
+                                              )
+                                       ) %>%
+    add_column(variable = vars_names,
+               .before = 1) %>%
+    select(-c(v, perc_na)) %>% 
+    add_column(type = 'Urban',
+               .before = 1) 
+
+cbgs_rural <- cbgs %>% filter(!urban)
+# Create rural-locations summary table
+cbgs_rural_summary_table <- bind_rows(lapply(vars,
+                                             function(x) {
+                                                 simple_summary(cbgs_rural, x)
+                                             }
+                                             )
+                                      ) %>%
+    add_column(variable = vars_names,
+               .before = 1) %>%
+    select(-c(v, perc_na)) %>% 
+    add_column(type = 'Rural',
+               .before = 1)
+
+cbgs_summary_table <- rbind(cbgs_urban_summary_table, 
+                            cbgs_rural_summary_table) %>%
+    arrange(desc(type)) %>%
+    select(-type)
+    
+# Rename columns:
+colnames(cbgs_summary_table) <- c('',
+                                  'Q10',
+                                  'Q25',
+                                  'Med', 
+                                  'Q75',
+                                  'Q90',
+                                  'Mean',
+                                  'SD')
+
+# Save the summary statistics table
+outfile = file.path(tables_folder_path, 'locations_summary_table.tex')
+
+# Create table
+cbgs_summary_table_latex <- kable(cbgs_summary_table,
+                                  format = 'latex',
+                                  caption = 'Locations (CBGs) summary statistics.',
+                                  digits = 2,
+                                  booktabs = TRUE) %>%
+    row_spec(row = 0, bold = TRUE) %>%
+    pack_rows(index = c('Urban' = 5, 'Rural' = 5)) %>%
+    add_footnote(paste('\\scriptsize{\\emph{Note}: Subset of data for',
+                       month,
+                       paste0(year, '.}')),
+                 notation = 'none',
+                 escape = FALSE) 
+# Fix caption
+add_to_beginning <- '\\begingroup\\footnotesize\n'
+add_to_end <- '\\endgroup\n\\caption{Locations (CBGs) summary statistics.}\n\\label{location_summary_table}\n'
+cbgs_summary_table_latex <- gsub('\n\n\\\\caption\\{[^\n]*\\}\n', '\n', cbgs_summary_table_latex)
+cbgs_summary_table_latex <- gsub('\\centering\n', 
+                                 paste0('\\centering\n',
+                                        add_to_beginning), 
+                                 cbgs_summary_table_latex,
+                                 fixed = TRUE)
+cbgs_summary_table_latex <- gsub('\\end{tabular}\n',
+                                 paste0('\\end{tabular}\n',
+                                        add_to_end
+                                        ),
+                                 cbgs_summary_table_latex,
+                                 fixed = TRUE)
+cbgs_summary_table_latex %>% cat(file = outfile)
+
+################################################################################
