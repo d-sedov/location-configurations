bef5345aa8058b0e7f72231521774be4b9ff626d
Dmitry Sedov <d-sedov@outlook.com>
2020-04-09 01:15:19 +0000

    Zip code handling for loopnet added.

--- data-construction/rent-download/loopnet/handle_zip_codes.py
+++ data-construction/rent-download/loopnet/handle_zip_codes.py
@@ -55,8 +55,10 @@ def create_zip_codes_table():
     print(f'{zip_codes_table.shape[0]} zip codes in total.')
 
     zip_codes_table['downloaded'] = 0
+    zip_codes_table['initiated'] = 0
 
-    zip_codes_table.sort_values(by = ['count'], ascending = False, inplace = True)
+    zip_codes_table.sort_values(by = ['count', 'zip_code'], ascending = [False,
+        True], inplace = True)
 
     print('Saving csv to disk.')
     zip_codes_table.to_csv(os.path.join(output_folder_path, 'zip_codes.csv'),
@@ -66,27 +68,60 @@ def create_zip_codes_table():
 
 def save_zip_part_for_download(n, name):
 
-    zip_codes_table = pd.read_csv(os.path.join(output_folder_path, 'zip_codes.csv'))
-    zip_codes_table = zip_codes_table[zip_codes_table['downloaded'] == 0]
-    zip_codes_table.sort_values(by = ['count', 'zip_code'], ascending = [False, True], inplace = True)
-    to_do = zip_codes_table.head(n)
+    print('Exporting part.')
+    zip_codes_table = pd.read_csv(os.path.join(output_folder_path,
+        'zip_codes.csv'), dtype = {'zip_code': str, 'initiated': int,
+            'downloaded': int})
+    candidates = (zip_codes_table['downloaded'] == 0) & (zip_codes_table['initiated'] == 0)
+    part_table = zip_codes_table[candidates].copy()
+    part_table.sort_values(by = ['count', 'zip_code'], ascending = [False, True], inplace = True)
+    to_do = (part_table.head(n)).copy()
+
+    to_do['initiated'] = 1
 
     to_do.to_csv(os.path.join(to_do_folder_path, name), index = False)
+    print('Done.')
+
+    zip_codes_table.set_index('zip_code', inplace = True)
+    to_do.set_index('zip_code', inplace = True)
+    zip_codes_table.update(to_do)
+    zip_codes_table['downloaded'] = zip_codes_table['downloaded'].astype(int)
+    zip_codes_table['initiated'] = zip_codes_table['initiated'].astype(int)
+    zip_codes_table['count'] = zip_codes_table['count'].astype(int)
+    zip_codes_table.reset_index(inplace = True)
+    zip_codes_table.sort_values(by = ['count', 'zip_code'], ascending = [False,
+        True], inplace = True)
+
+    print('Updating zip table on disk.')
+    zip_codes_table.to_csv(os.path.join(output_folder_path, 'zip_codes.csv'),
+            index = False)
     print('Done.')
 
     
 def update_zip_codes_table(name):
 
-    zip_codes_table = pd.read_csv(os.path.join(output_folder_path, 'zip_codes.csv'))
+    print('Getting current table...')
+    zip_codes_table = pd.read_csv(os.path.join(output_folder_path,
+        'zip_codes.csv'), dtype = {'zip_code': str, 'initiated': int,
+            'downloaded': int})
     zip_codes_table.set_index('zip_code', inplace = True)
-    update_table = pd.read_csv(os.path.join(update_folder_path, name))
+    print('Getting updates...')
+    update_table = pd.read_csv(os.path.join(update_folder_path, name), dtype = {'zip_code': str, 'initiated': int,
+            'downloaded': int})
     update_table.set_index('zip_code', inplace = True)
 
+    print('Writing updates...')
     zip_codes_table.update(update_table)
+    zip_codes_table['downloaded'] = zip_codes_table['downloaded'].astype(int)
+    zip_codes_table['initiated'] = zip_codes_table['initiated'].astype(int)
+    zip_codes_table['count'] = zip_codes_table['count'].astype(int)
     zip_codes_table.reset_index(inplace = True)
+    zip_codes_table.sort_values(by = ['count', 'zip_code'], ascending = [False,
+        True], inplace = True)
 
     zip_codes_table.to_csv(os.path.join(output_folder_path, 'zip_codes.csv'),
             index = False)
+    print('Done.')
 
 
 ################################################################################
